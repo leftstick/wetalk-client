b880742a2a0018fb145ccd15c4df67277204c90a
Howard.Zuo <leftstick@qq.com>
2016-01-05 15:45:32 +0800

    fix issue that windows os cannot debug the GUI

--- gulpfile.js
+++ gulpfile.js
@@ -134,7 +134,7 @@ gulp.task('release', ['compile-release'], function() {
 gulp.task('dev', function(cb) {
     var isWin = /^win/.test(process.platform);
     require('child_process')
-        .exec(isWin ? 'sh' : 'node' + ' ./node_modules/.bin/electron --debug=5858 ./src/build/', {
+        .exec((isWin ? 'sh' : 'node') + ' ./node_modules/.bin/electron --debug=5858 ./src/build/', {
             cwd: __dirname,
             env: {
                 NODE_ENV: 'dev'
